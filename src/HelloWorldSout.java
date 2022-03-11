8d7daaa9ec660a46fe8eb8a42aea4394d61562d3
CodeClod <mika9103@stud.kea.dk>
2022-03-11 10:05:54 +0100

    Added 1-10 sout method

--- src/HelloWorldSout.java
+++ src/HelloWorldSout.java
@@ -1,7 +1,17 @@
 public class HelloWorldSout {
+
+    void printOneToTen() {
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Just a text");
         System.out.println("Hello World 2");
+
+        new HelloWorldSout().printOneToTen();
+
     }
 }
