1d7214af393ae657b31e46c40494b8a6947325c8
Tobias <tobiasurban@hotmail.com>
2022-03-11 10:07:24 +0100

    added method

--- src/HelloWorldSout.java
+++ src/HelloWorldSout.java
@@ -1,5 +1,13 @@
 public class HelloWorldSout {
+
+    void metode2 () {
+
+    }
+
+
     public static void main(String[] args) {
+        new HelloWorldSout().metode2();
+
         System.out.println("Hello World");
         System.out.println("Just a text");
         System.out.println("Hello World 2");
